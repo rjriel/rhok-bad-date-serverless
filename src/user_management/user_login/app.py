b1a044c104b25e6e7bfa6807d0690f1028f20ef5
Rene-Jacques Riel <rie@qlik.com>
2018-04-14 16:29:24 -0400

    adding user_login lambda

---
+++ src/user_management/user_login/app.py
@@ -0,0 +1,23 @@
+import boto3
+import hashlib
+from boto3.dynamodb.conditions import Key
+
+def lambda_handler(event, context):
+  hashed_passwd = hash_password(event['password'])
+  dynamodb = boto3.resource('dynamodb', region_name='ca-central-1')
+  table = dynamodb.Table('user')
+  response = table.query(
+    KeyConditionExpression=Key('username').eq(event['username'])
+  )
+  if len(response):
+    if response['Items'][0]['password'] == hashed_passwd:
+      return True
+    else:
+      return False
+  else:
+    return False
+
+def hash_password(passwd):
+  passwd_hash = hashlib.sha256()
+  passwd_hash.update(passwd.encode('utf-8'))
+  return passwd_hash.digest()
