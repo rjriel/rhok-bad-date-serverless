ad5416fe7870691b254ebc6aaf52e0c77b63f1bf
Jacob Rahme <jacob@jrahme.ca>
2018-04-14 17:50:54 -0400

    update formatting and use environment

--- src/user_management/user_login/app.py
+++ src/user_management/user_login/app.py
@@ -7,22 +7,32 @@ from boto3.dynamodb.conditions import Key
 def lambda_handler(event, context):
   hashed_passwd = hash_password(event['password'])
   dynamodb = boto3.resource('dynamodb', region_name='ca-central-1')
-  table = dynamodb.Table('user')
+  table = dynamodb.Table(os.getenv('USER_TABLE', 'user'))
+  username =  event['name']
   response = table.query(
-    KeyConditionExpression=Key('username').eq(event['name'])
+    KeyConditionExpression=Key('username').eq(username)
   )
-  if len(response):
-    if response['Items'][0]['password'] == hashed_passwd:
+  
+  if response.get('Items'):
+    if response['Items'][0].get('password') == hashed_passwd:
       token = str(uuid.uuid4())
       token_table = dynamodb.Table('token')
-      token_table.put_item(Item={'username': event['name'], 'token': token, 'ttl': 3600 })
+      token_table.put_item(Item={'username': username,
+                                'token': token,
+                                'ttl': int(os.getenv('ttl', 3600))})
       return token
+
     else:
       return False
-  else:
-    return False
+
+  return False
 
 def hash_password(passwd):
   passwd_hash = hashlib.sha256()
   passwd_hash.update(passwd.encode('utf-8'))
   return passwd_hash.hexdigest()
+
+# testing hook
+if __name__ == '__main__':
+  os.environ['ttl'] = '3600'
+  lambda_handler({'name' : 'test2', 'password' : 'test'}, {})
