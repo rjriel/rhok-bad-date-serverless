d6ce85c22f920d4796c0d1209e37047799e83666
Jacob Rahme <jacob@jrahme.ca>
2018-04-14 16:17:13 -0400

    test code

--- src/user_management/user_register/app.py
+++ src/user_management/user_register/app.py
@@ -1,9 +1,26 @@
 #/usr/bin/python3
 
 import boto3
-import boto-core
+import hashlib
 import os
 
+def hash_password(passwd):
+  passwd_hash = hashlib.sha256()
+  passwd_hash.update(passwd.encode('utf-8'))
+  return passwd_hash.digest()
 
+def insert_user():
+  pass
+
+def check_user(client, username):
+    response = client.get_item(TableName='user',
+        Key={'username' : {'S': username}})
+    
+    print(response)
+
+  
 def lambda_handler(event, ctx):
-  iam = boto3.connect()
+  #gross, find a way to not hard code that 
+  dynamo_client = boto3.client('dynamodb')
+  check_user(dynamo_client, 'test')
+
