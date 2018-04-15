049313610428d4712115b1a0142723d974d6e2c6
Jacob Rahme <jacob@jrahme.ca>
2018-04-15 09:37:17 -0400

    add debugging to user registration

--- src/user_management/user_register/app.py
+++ src/user_management/user_register/app.py
@@ -8,11 +8,13 @@ import os
 def hash_password(passwd):
   passwd_hash = hashlib.sha256()
   passwd_hash.update(passwd.encode('utf-8'))
+  if os.getenv('DEBUG') is '1':
+    print('password hash is {}'.format(passwd_hash.hexdigest))
   return passwd_hash.hexdigest()
 
 def create_user(client, event):
   passwd = hash_password(event['password'])
-  client.put_item(TableName='user', Item={'username' : {'S' : event['name']},
+  client.put_item(TableName='user', Item={'user_name' : {'S' : event['name']},
                       'password' : {'S' : passwd},
                       'role' : {'S' : 'submitter'}
   })
@@ -20,8 +22,9 @@ def create_user(client, event):
 
 def user_exists(client, username):
     response = client.get_item(TableName='user',
-        Key={'username' : {'S': username}})
-    
+        Key={'user_name' : {'S': username}})
+    if os.getenv('DEBUG') is '1':
+      print('dynamo response is \n {}'.format(response))
     if not 'Item' in response:
       #the user does not exist
       return False
@@ -31,10 +34,20 @@ def user_exists(client, username):
 
   
 def lambda_handler(event, ctx):
-  #gross, find a way to not hard code that 
+  debug = os.getenv('DEBUG') is '1'
+  if debug:
+    print('EVENT*******')
+    print(event)
   dynamo_client = boto3.client('dynamodb')
   if not  user_exists(dynamo_client, event['name']):
+    if debug:
+      print('creating user {}'.format(event['name']))
     create_user(dynamo_client, event)
     return { 'success': True }
   else:
     return { 'success': False, 'message': "Username already exists" }
+
+
+if __name__ == '__main__':
+  os.environ['DEBUG'] = '1'
+  lambda_handler({'name' : str(uuid4()), 'password' : 'password1234'}, {})
